cccb6669d7586593a415131fb0f84d925e4b2bcf
abar.toha <abar.toha@gmail.com>
2024-11-22 04:13:21 +0600

    working! Primitive

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import json
 from fuzzywuzzy import fuzz
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QListWidget, QVBoxLayout, QSizePolicy
-
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QListWidget, QVBoxLayout, QSizePolicy, QMessageBox
+THRES = 90
 def search_json(data, query):
     results = []
     for item in data:
-        if fuzz.partial_ratio(query.lower(), item['title'].lower()) >= 70:
+        if fuzz.partial_ratio(query.lower(), item['title'].lower()) >= THRES or \
+           any(fuzz.partial_ratio(query.lower(), tag.lower()) >= THRES for tag in item['genre']):
             results.append(item)
     return results
 
@@ -42,6 +43,7 @@ class SearchApp(QWidget):
 
         # Connect button click to search function
         self.search_button.clicked.connect(self.search)
+        self.result_list.itemDoubleClicked.connect(self.show_details)
 
     def search(self):
         query = self.search_box.text()
@@ -50,6 +52,24 @@ class SearchApp(QWidget):
         self.result_list.clear()
         for result in results:
             self.result_list.addItem(result['title'])
+
+    def show_details(self):
+        selected_item = self.result_list.currentItem()
+        if selected_item:
+            item_text = selected_item.text()
+            for result in self.data:
+                infos = ""
+                for text in result['features']:
+                    infos += text + "\n"
+                if result['title'] == item_text:
+                    # Display details in a message box
+                    QMessageBox.information(self, "Details", f"Title: {result['title']}\n"
+                                                                 f"Release Date: {result['date']}\n"
+                                                                 f"Release Date: {result['genre']}\n"
+                                                                 f"Download Links: {infos}\n"
+                                                                 # Add other fields as needed
+                                                                 )
+                    break
 
 if __name__ == '__main__':
     app = QApplication([])
